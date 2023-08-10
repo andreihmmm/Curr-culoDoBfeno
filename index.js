ffa6ad5748d73f929f122214ed02ac2baa0a30dc
Andrei Alves Fracalossi <124471216+andreihmmm@users.noreply.github.com>
2023-08-10 18:41:24 -0300

    Atualização da página
    alterei o html, criei um css e um script que interage com um botão.

---
+++ index.js
@@ -0,0 +1,6 @@
+function teste() {
+    botao = document.querySelector("#clique")
+    botao.classList.add("desativado")
+    habilidades = document.querySelector("#habilidades");
+    habilidades.classList.add("ativado")
+}
